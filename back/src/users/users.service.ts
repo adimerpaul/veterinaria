4281c961f4d392993e73385ea5447394a0b4298b
adimer <adimer101@gmail.com>
2025-02-28 04:09:31 -0400

    Actulizado para user create

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+  async create(body) {
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    const user = this.usersRepository.create({
+      ...body,
+      password: hashedPassword,
+    });
+    return this.usersRepository.save(user);
   }
 
   login(createUserDto: CreateUserDto) {
